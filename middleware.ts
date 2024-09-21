655d15779eee2221c56d9de22448422a88345328
iplatos <platonoff9168@gmail.com>
2024-09-21 23:54:11 +0300

    feat:add middleware for redirect

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,17 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-// Функция для получения токена из cookies
 function getAuthToken(request: NextRequest): string | null {
-  const token = request.cookies.get('accessToken') // Получаем токен
-  return token ? token.value : null // Возвращаем значение токена
+  const token = request.cookies.get('accessToken')
+  return token ? token.value : null
 }
 
 export function middleware(request: NextRequest) {
   const token = getAuthToken(request)
   const { pathname } = request.nextUrl
 
-  // Если пользователь авторизован и находится на странице входа, перенаправляем его на главную
   if (pathname === '/signIn' && token) {
-    return NextResponse.redirect(new URL('/', request.url))
+    return NextResponse.redirect(new URL('/my-info', request.url))
   }
 
   if (pathname !== '/signIn' && pathname !== '/public-page' && !token) {
