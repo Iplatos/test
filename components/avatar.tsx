3028038555ffc1bf0676c19fd9cf52aade82ad62
iplatos <platonoff9168@gmail.com>
2024-09-23 13:16:32 +0300

    feat: created Avatar component

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -1,8 +1,6 @@
-import { MY_PROFILE_QUERY } from '@/apollo/myProfile'
-import { useQuery } from '@apollo/client'
 import Image from 'next/image'
 import { ComponentPropsWithoutRef } from 'react'
 
 export const Avatar = (props: ComponentPropsWithoutRef<typeof Image>) => {
-  return <Image {...props} />
+  return <Image className="rounded-full z-50 " {...props} />
 }
